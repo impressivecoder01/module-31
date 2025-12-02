6cef624e287f214234204c36d77e06c4b444801a
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-03 01:09:21 +0600

    class four

--- classFour.js
+++ classFour.js
@@ -1,1 +1,8 @@
 // double equal(==) vs triple equal(===), implicit conversion
+console.log(2===3)
+console.log(2===2)
+console.log(true === 1)
+console.log(false===0)
+console.log(null===undefined)
+console.log(0==='')
+// console.log([]==='')
