edc45951a9e99a10e0c68e317b9c032ee7512376
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-03 02:48:54 +0600

    class 8

--- classEight.js
+++ classEight.js
@@ -1,1 +1,10 @@
 // (advanced) explore about closure
+function counter (){
+    let count =0;
+    function inner(){
+        console.log('called')
+    }
+    return inner
+}
+const out = counter()
+console.log(out())
--- classSeven.js
+++ classSeven.js
@@ -1,1 +1,19 @@
 // (advanced) function arguments pass by reference pass by value
+const manik = {nam: 'manik', balance :20000};
+const bosir = {nam: 'bosir', balance: 50000}
+function money(a,b){
+    a.balance = 0
+    b.balance = b.balance/2
+    const total = a.balance + b.balance
+    return total
+}
+console.log(money(manik,bosir))
+console.log(manik,bosir)
+
+function add(a,b){
+    // console.log(a,b)
+    console.log(arguments)
+    const params = [...arguments]
+    console.log(params)
+}
+add(88,44,55,4)
