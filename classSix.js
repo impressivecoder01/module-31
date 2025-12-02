c495f5967dd8ae3899e904791cbc5189f891e767
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-03 01:32:48 +0600

    class Six

--- classSix.js
+++ classSix.js
@@ -1,1 +1,10 @@
 // (optional) callback function and pass different functions
+function bai(a,b,c){
+    if(c){
+        console.log(a)
+    }
+}
+function call(person){
+    console.log('calling', person)
+}
+call()
