701ebc3910b272dcbd35f80d0efb49db1410de00
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-03 01:22:29 +0600

    class five

--- classFive.js
+++ classFive.js
@@ -1,1 +1,19 @@
 // block scope global scope, simple understanding of hosting
+const nam = 'tom'
+if(true){
+    const data = 58;
+    console.log(data,nam)
+}
+// console.log(data)
+
+for(const num of [1,2,3,4,5]){
+    console.log(num)
+}
+
+//function scope or local scope
+function doMath(a,b){
+    const sum = a + b
+    console.log(sum,a,b,nam)
+}
+// console.log(a,b)
+
