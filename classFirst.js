05bd1638378b08bf58fcfacec9d782f13e6cc18c
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-02 23:08:14 +0600

    class one added

--- classFirst.js
+++ classFirst.js
@@ -1,1 +1,15 @@
 // module overview, primitive and non-primitive data type
+let data = 4;
+data = 'a'
+data = false
+console.log(typeof data)
+// js is a dynamic typed programming language
+// js primitive data type
+const price = 50
+const nam ='a'
+const isPoor = false;
+let isKnown = undefined // usually you should not set value to undefined
+let is = null
+// js non-primitive data type
+const student = {nam: 'b', marks: 33,}
+const friends =  ['a', 'b', 'c']
